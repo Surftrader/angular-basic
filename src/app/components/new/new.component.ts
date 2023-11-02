a2602b31d51db5daa619e678777f88d7b96094de
Sergiy Poddubka <poseal@gmail.com>
2023-11-02 19:21:21 +0200

    create new component

---
+++ src/app/components/new/new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-app',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.css']
+})
+export class NewComponent {
+
+}
