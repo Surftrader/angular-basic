ab724d84840487cf099426269a1bd50dd41abb44
Sergiy Poddubka <poseal@gmail.com>
2023-11-02 17:05:51 +0200

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello World from TS</h1>`,
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: ['h1 {color: blue}']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  interpolation: ['[', ']']
 })
 export class AppComponent {
   title = 'angular-basic';
