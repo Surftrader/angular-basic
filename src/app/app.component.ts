f8be2a06ff4a11cdf9a691bb395d0403fa32f911
Sergiy Poddubka <poseal@gmail.com>
2023-11-02 16:30:10 +0200

    page and styles from ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello World from TS</h1>`,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles: ['h1 {color: blue}']
 })
 export class AppComponent {
   title = 'angular-basic';
